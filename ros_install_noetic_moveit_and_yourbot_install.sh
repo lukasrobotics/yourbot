c60f90f0136ef41884f203254c16ea9bc5d55951
lukasrobotics <94325853+lukasrobotics@users.noreply.github.com>
2021-12-14 17:25:24 +0100

    Update ros_install_noetic_moveit_and_yourbot_install.sh

--- ros_install_noetic_moveit_and_yourbot_install.sh
+++ ros_install_noetic_moveit_and_yourbot_install.sh
@@ -1,11 +1,3 @@
-#!/bin/bash -eu
-
-# The BSD License
- 
-# Copyright (c) 2020 Qbotics Labs Pvt Ltd
-# Copyright (c) 2014 OROCA and ROS Korea Users Group
-
-
 
 name_ros_distro=noetic 
 user_name=$(whoami)
